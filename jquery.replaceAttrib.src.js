eac7a126ca7ae62d62bc0eefb2309e92322cf1ce
dkarandana <dhananjayakarandana@gmail.com>
2015-08-07 11:27:35 +0530

    jquery.replaceAttrib.src.js
    Non compressed version

---
+++ jquery.replaceAttrib.src.js
@@ -0,0 +1,17 @@
+$.fn.replaceAttrib = function( data , removeOriginal ){
+  return this.each(function(){
+    var ele = $(this);
+     for(var prop in data ){ 
+
+       if(typeof ele == 'object' && typeof ele.attr(prop) !== 'undefined'){
+		   
+		    console.log('replaceAttr',ele, prop, data[prop],ele.attr(prop)) ;
+			ele.attr(data[prop],ele.attr(prop));
+		  
+			if(removeOriginal !== false){
+				ele.removeAttr(prop);
+			}
+       }
+     }
+  });
+};
